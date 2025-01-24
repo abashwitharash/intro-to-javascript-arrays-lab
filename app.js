da2abfdb63d20c3f636283a378242d3d3c1aba48
Arash <arashsaleh798@gmail.com>
2025-01-23 21:18:02 -0800

    Exercise 11 is complete and was not easy

--- app.js
+++ app.js
@@ -52,15 +52,47 @@ const soyIdx = (foods.indexOf("tofu"))
 
 console.log('Exercise 8 result:', soyIdx);
 
-// Exercise 9 
+// Exercise 9 Join
 
 const allFoods = foods.join('->');
 
 console.log('Exercise 9 result:', allFoods);
 
-// Exercise 10
+// Exercise 10 Check for an element 
 
 const hasSoup = foods.includes("soup");
 
 console.log('Exercise 10 result:', hasSoup);
+
+/* for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+*/
+
+
+/*
+Exercise 11: Odd numbers from an array
+
+1) Choose a method to iterate through the `nums` array.
+
+2) Push each odd number to a new array named `odds`.
+
+Hint: Initialize the `odds` variable to an empty array before the iteration.
+
+Complete Exercise 11 in the space below:
+*/
+
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]
+const odds = []; {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 !== 0) {
+            odds.push(nums[i]);
+        }
+
+    }
+}
+
+console.log('Exercise 11 result:', odds);
+
+
 
